e2f4ff9fc7bf84e3abaa3c83e15d9a8476cd9b65
DiboraEyasuE <diboraeyasu792@gmail.com>
2025-12-23 16:51:30 +0300

    feat: feature engineering finished

---
+++ scripts/feature_engineering.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+class FeatureEngineering:
+    def __init__(self, df):
+        self.df = df.copy()
+
+
+    def add_time_features(self):
+        self.df["time_since_signup"] = (self.df["signup_time"] - self.df["purchase_time"]).dt.total_seconds()
+        self.df["hour_of_day"] = self.df["purchase_time"].dt.hour
+        self.df["day_of_week"] = self.df["purchase_time"].dt.dayofweek
+        return self.df
+    
+
+    def add_transaction_frequency(self):
+        txn_count = self.df.groupby("user_id")["purchase_time"].count()
+        self.df["txn_per_user"] = self.df["user_id"].map(txn_count)
+        return self.df
+
